7c5986e3e4b0e3eac856964744696013e4718ad4
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-15 11:34:17 -0800

    report bug to node.js

---
+++ components/BrewerySearch/brewery-search.component.ts
@@ -0,0 +1,78 @@
+import { defineComponent, html } from '@tybalt/core';
+import { derive, reactive } from '@tybalt/reactive';
+
+import css from './brewery-search.css';
+
+const BASE_URL = 'https://api.openbrewerydb.org/v1/breweries';
+
+export default defineComponent({
+    name: 'oq-brewery-search',
+    css,
+    setup() {
+        const city = reactive('');
+        const cities = reactive([]);
+        const loading = reactive(false);
+
+        const clickHandler = async () => {
+            loading.value = true;
+
+            const results = await fetch(`${BASE_URL}?by_city=${city.value}`);
+            const json = await results.json();
+            cities.value = json;
+
+            loading.value = false;
+        };
+
+        const inputHandler = (evt: Event) => {
+            city.value = evt.target?.value;
+        };
+
+        const loadingMessage = derive(loading, ([value]) => {
+            if (value) {
+                return html`<div>Searching for ${city.value}...</div>`;
+            } else {
+                return '';
+            }
+        });
+
+        const citiesList = derive(cities, ([value]) => {
+            if (cities.length) {
+                const lis = value.map(
+                    (localCity) => html`
+                        <li>
+                            <a href="${localCity.website_url}">${localCity.name}</a>
+                        </li>
+                    `,
+                );
+                return html`
+                    <ul>
+                        ${lis}
+                    </ul>
+                `;
+            } else {
+                return '';
+            }
+        });
+
+        return {
+            inputHandler,
+            clickHandler,
+            city,
+            citiesList,
+            loadingMessage,
+        };
+    },
+    render({ city, citiesList, inputHandler, clickHandler, loadingMessage }) {
+        return html`
+            <div class="brewery-search">
+                ${loadingMessage}
+                ${citiesList}
+                <label>
+                    <span>City name</span>
+                    <input type="text" @input="${inputHandler}" value="${city}"></input>
+                </label>
+                <example-button @click="${clickHandler}">Search</example-button>
+            </div>
+        `;
+    },
+});
--- components/HelloWorld/hello-world.ts
+++ components/HelloWorld/hello-world.ts
@@ -2,10 +2,9 @@ import { defineComponent, html } from '@tybalt/core';
 import { compose, string, required } from '@tybalt/validator';
 
 export default defineComponent({
-    name: 'hello-world',
-    
+    name: 'oq-hello-sayer',
     props: { name: { validator: compose(string(), required) } },
-    render({ name }, ctx) {
-        return html`<div class="hello-world">Hello ${name}</div>`;
+    render({ name }) {
+        return html`<div class="hello-sayer">Hello ${name}</div>`;
     }
 });
